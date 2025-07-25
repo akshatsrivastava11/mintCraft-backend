c04b5d7cbf4ebee4c7bff66ce7dfe178a1b75a0a
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-07-25 20:30:43 +0530

    aiModelRouter

--- src/trpc/routers/aiModelRouter.ts
+++ src/trpc/routers/aiModelRouter.ts
@@ -1,24 +1,62 @@
 import {z} from 'zod'
 import { procedures, router } from '..'
-import { RegisterAIModelSchema } from '../schemas/registerAIModelSchema'
 import { TRPCError } from '@trpc/server'
 import {PrismaClient} from '../../database/generated/prisma'
+import { RegisterAIModelSchema } from '../schemas/registerAIModelSchema'
+import {registerAiModel} from '../../../clients/generated/umi/src'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import { generateSigner } from '@metaplex-foundation/umi'
+import { SYSTEM_PROGRAM_ADDRESS } from 'gill/programs'
+import { PublicKey } from '@solana/web3.js'
 const prismaClient=new PrismaClient()
-
+const umi=createUmi("https://api.devnet.solana.com");
 export const aiModelRouter=router({
     //register a new Ai Model
     register:procedures.input(RegisterAIModelSchema)
             .mutation(async({input,ctx})=>{
                 try {
-                    
-                    //sends the transaction 
-                    
                     if(!ctx.user){
                         throw new TRPCError({
                             code: 'UNAUTHORIZED',
                             message: 'You must be logged in to register an AI Model.'
                         })
                     }
+                    if(input.aiModelPublicKey==undefined){
+                        throw new TRPCError({
+                            code: 'BAD_REQUEST',
+                            message: 'AI Model Public Key is required.'
+                        })
+                    }
+//seeds=[b"ai",name.as_bytes(),signer.key().as_ref(),global_state.key().as_ref()],
+
+                    let [aiModelPda,bump]=PublicKey.findProgramAddressSync(
+                        [Buffer.from("ai"),ctx.user.wallet.toBuffer(),globalState.toBuffer()],
+                    AI_MODEL_PROGRAM_ID
+                    )
+                    const aiModelPdaWithBump{
+                        publicKey:aiModelPda,
+                        bump:bump
+                    };
+
+                    const id=
+                    //sends the transaction 
+                   const aiModelAccount=generateSigner(umi);
+                   const transactionBuilder=registerAiModel(umi,{
+                    name:input.name,
+                    description:input.description,
+                    apiEndpoint:input.apiEndpoint,
+                    royaltyPercentage:input.royaltyPerGeneration,
+                    aiModel:aiModelPdaWithBump,
+                    signer:ctx.user.wallet,
+                    id:,
+                    globalState:,
+                    systemProgram:SYSTEM_PROGRAM_ADDRESS,
+                    userConfig:,
+                    })
+
+
+
+
                     //create an ai model in the database
                     const aiModel=await prismaClient.aIModel.create({
                         data:{
@@ -29,7 +67,7 @@ export const aiModelRouter=router({
                             royaltyPercentage:input.royaltyPerGeneration,
                             isActive:true,
                             aiModelPublicKey:input.aiModelPublicKey,
-                            
+
                         },
                         include:{
                             owner:{
